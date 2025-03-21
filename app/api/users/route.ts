e8d1a370c85f034a42107dfa7bfdd7eb7a249c88
muhidtech <muhideen894@gmail.com>
2025-03-21 10:11:04 +0000

    ecoHaven

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { promises as fs } from 'fs';
 import path from 'path';
 
-interface User {
+interface User extends Record<string, unknown> {
   id: string;
   username: string;
   firstName: string;
